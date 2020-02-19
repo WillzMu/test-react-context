9d370288c42bc0cc20a8a9e505fa6a7880cd71db
WillzMu <wilfredmulenga@yahoo.com>
2020-02-19 16:23:07 +0200

    refactor: test context api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const MyContext = React.createContext()
+
+class MyProvider extends React.Component {
+  state = { name: 'Wilfred', age: 27 }
+
+  render() {
+    return (
+      <MyContext.Provider value={{
+        state: this.state,
+        growAYearOlder: () => this.setState({ age: this.state.age + 1 })
+      }}>
+        {this.props.children}
+      </MyContext.Provider>
+    )
+  }
+}
+
+function Person() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MyContext.Consumer>
+        {(context) => (
+          <>
+            <p>I am {context.state.name}</p>
+            <p>And I am {context.state.age}</p>
+            <button onClick={context.growAYearOlder}>Increase age</button>
+          </>
+        )}
+      </MyContext.Consumer>
     </div>
+  )
+}
+
+function App(props) {
+  return (
+    <MyProvider>
+      <div>
+        <Person />
+      </div>
+    </MyProvider>
   );
 }
 
